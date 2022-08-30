94d0a04cba94255811c8ade17038d72da7f9aef2
Novesh Goyal <>
2022-08-30 10:37:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+var readlinesync = require('readline-sync');
+var userName = readlinesync.question("What's your name? ");   // input user's name
+var score = 0;
+
+console.log(`Welcome ${userName} to DO YOU KNOW Novesh?`);  // Welcome message
+
+
+var questions = [{
+  question: "What is my birth-month? ",
+  answer: "july"
+}, {
+  question: "In which state I was born? ",
+  answer: "maharashtra"
+}]
+
+function play(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    var userAns = readlinesync.question(arr[i].question);
+    if (userAns.toUpperCase() === arr[i].answer.toUpperCase()) {
+      console.log(`awesome!`);
+      score += 1;
+    }
+    else console.log(`wrong!`);
+    console.log(`current score: ${score}`);
+    console.log("-----------------");
+  }
+
+}
+play(questions);
